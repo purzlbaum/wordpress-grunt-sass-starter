58bdf8894ccf2ffef5bb5e10345389531a65222e
Claudio Schwarz <claudio.schwarz@guavestudios.com>
2015-05-11 16:04:25 +0200

    Init

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+
+  // 1. All configuration goes here
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      dist: {
+        src: [
+          'js/plugins/jquery.min.js',
+          'js/plugins/owl.carousel.min.js',
+          'js/bootstrap/collapse.js',
+          'js/bootstrap/dropdown.js',
+          'js/bootstrap/transition.js',
+          'js/themefunctions.js'
+        ],
+        dest: 'js/theme.js'
+      }
+    },
+
+    uglify: {
+      build: {
+        src: 'js/theme.js',
+        dest: 'js/theme.min.js'
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['concat', 'uglify'],
+        options: {
+          spawn: false
+        }
+      },
+      css: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'css/default.css': 'sass/default.sass'
+        }
+      }
+    }
+
+  });
+
+  // 3. Where we tell Grunt we plan to use this plug-in.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+  grunt.registerTask('default', ['concat', 'uglify', 'watch', 'sass']);
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # wordpress-grunt-sass-starter
 Starter Theme for WordPress - comes with Grunt, Bootstrap and SASS
+
+## Init
+
+Run `npm install` (or `sudo npm install`) for a first run.
+
+## Commands
+
+`grunt default` - to run all tasks
+`grunt concat` - concatenate all files
+`grunt uglify` - minify all files
+`grunt watch` - watch js and sass files
